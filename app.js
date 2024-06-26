a4c3bfc4fc15f8692add921372c7d0155944dfe3
Markedia Hinds <mhinds@pursuit.org>
2024-06-26 08:57:28 -0400

    Create all CRUD Routes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 
 const logsController = require('./controllers/logsController')
-
+const v2logsCOntroller = require('../v2/controllers/logsController')
 const app = express()
 
 app.use(express.json())
 
 app.use('/logs', logsController)
+app.use('/v2/logs', v2logsCOntroller)
 
 app.get('/', (req, res) => {
     res.send(`Welcome to M.O.K.A Captain Logs`)
